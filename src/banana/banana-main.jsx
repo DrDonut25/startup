3bd799cdbd4831c6186f792d9a3a27f705078193
DrDonut25 <remundparker@gmail.com>
2025-10-17 22:30:50 -0600

    Filled in change cells for banana & button-styled upgrade headers

--- src/banana/banana-main.jsx
+++ src/banana/banana-main.jsx
@@ -52,12 +52,12 @@ export function BananaMain() {
             <table className="upgrade_table">
                 <thead>
                     <tr>
-                        <th className="top_row">Top Upgrade Path</th>
-                        <th className="top_row">Increased Production</th>
-                        <th className="top_row">Greater Production</th>
-                        <th className="top_row">Banana Plantation</th>
-                        <th className="top_row">Banana Research Facility</th>
-                        <th className="top_row">Banana Central</th>
+                        <th className="top_row path_label">Top Upgrade Path</th>
+                        <th className="top_row upgrade_button">Increased Production</th>
+                        <th className="top_row upgrade_button">Greater Production</th>
+                        <th className="top_row upgrade_button">Banana Plantation</th>
+                        <th className="top_row upgrade_button">Banana Research Facility</th>
+                        <th className="top_row upgrade_button">Banana Central</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -71,11 +71,38 @@ export function BananaMain() {
                     </tr>
                     <tr>
                         <th>Changes</th>
-                        <td>To be added</td>
-                        <td>To be added</td>
-                        <td>To be added</td>
-                        <td>To be added</td>
-                        <td>To be added</td>
+                        <td class="change_cell">
+                            <ul class="change_list">
+                                <li>Banana count/round 4 -&gt; 6</li>
+                                <li>Income/round $80 -&gt; 120</li>
+                            </ul>
+                        </td>
+                        <td class="change_cell">
+                            <ul class="change_list">
+                                <li>Banana count/round 6 -&gt; 8</li>
+                                <li>Income/round $120 -&gt; $160</li>
+                            </ul>
+                        </td>
+                        <td class="change_cell">
+                            <ul class="change_list">
+                                <li>Banana count/round 8 -&gt; 16</li>
+                                <li>Income/round $160 -&gt; $320</li>
+                            </ul>
+                        </td>
+                        <td class="change_cell">
+                            <ul class="change_list">
+                                <li><i>Produces 5 banana crates/round instead of bananas</i></li>
+                                <li>Banana crate value: $300</li>
+                                <li>Income/round $320 -&gt; $1,500</li>
+                            </ul>
+                        </td>
+                        <td class="change_cell">
+                            <ul class="change_list">
+                                <li>Banana crate value $300 -&gt; $1,200</li>
+                                <li><i>Banana Central buff: all other Banana Research Facilities receive +25% income</i></li>
+                                <li>Income/round $1,500 -&gt; $6,000</li>
+                            </ul>
+                        </td>
                     </tr>
                 </tbody>
             </table>
@@ -83,12 +110,12 @@ export function BananaMain() {
             <table className="upgrade_table">
                 <thead>
                     <tr>
-                        <th className="top_row">Middle Upgrade Path</th>
-                        <th className="top_row">Long Life Bananas</th>
-                        <th className="top_row">Valuable Bananas</th>
-                        <th className="top_row">Monkey Bank</th>
-                        <th className="top_row">IMF Loan</th>
-                        <th className="top_row">Monkey-Nomics</th>
+                        <th className="top_row path_label">Middle Upgrade Path</th>
+                        <th className="top_row upgrade_button">Long Life Bananas</th>
+                        <th className="top_row upgrade_button">Valuable Bananas</th>
+                        <th className="top_row upgrade_button">Monkey Bank</th>
+                        <th className="top_row upgrade_button">IMF Loan</th>
+                        <th className="top_row upgrade_button">Monkey-Nomics</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -102,11 +129,39 @@ export function BananaMain() {
                     </tr>
                     <tr>
                         <th>Changes</th>
-                        <td>To be added</td>
-                        <td>To be added</td>
-                        <td>To be added</td>
-                        <td>To be added</td>
-                        <td>To be added</td>
+                        <td class="change_cell">
+                            <ul class="change_list">
+                                <li>Banana lifespan 15s -&gt; 30s</li>
+                            </ul>
+                        </td>
+                        <td class="change_cell">
+                            <ul class="change_list">
+                                <li>+25% banana value</li>
+                            </ul>
+                        </td>
+                        <td class="change_cell">
+                            <ul class="change_list">
+                                <li>Cash now generated in bank instead of through bananas</li>
+                                <li>Income/round: $225, though +15% interest is added to this number at end of each round</li>
+                                <li>Interest rate resets after withdrawing money</li>
+                                <li>Max bank balance: $7,000</li>
+                            </ul>
+                        </td>
+                        <td class="change_cell">
+                            <ul class="change_list">
+                                <li>Bank capacity $7,000 -&gt; $10,000</li>
+                                <li><span class="ability">Ability: immediately generate $9,000, though $9,000 will also be applied
+                                     in debt—50% of all user income will go toward debt counter until it's paid off</span></li>
+                                <li><span class="ability">Ability cooldown: 85s (though you can't use this ability again until
+                                     this Banana Farm's debt is paid off)</span></li>
+                            </ul>
+                        </td>
+                        <td class="change_cell">
+                            <ul class="change_list">
+                                <li><span class="ability">Ability no longer gives debt</span></li>
+                                <li><span class="ability">Ability cooldown 85s -&gt; 60s (max 2 uses/round)</span></li>
+                            </ul>
+                        </td>
                     </tr>
                 </tbody>
             </table>
@@ -114,12 +169,12 @@ export function BananaMain() {
             <table className="upgrade_table">
                 <thead>
                     <tr>
-                        <th className="top_row">Bottom Upgrade Path</th>
-                        <th className="top_row">EZ Collect</th>
-                        <th className="top_row">Banana Salvage</th>
-                        <th className="top_row">Marketplace</th>
-                        <th className="top_row">Central Market</th>
-                        <th className="top_row">Monkey Wall Street</th>
+                        <th className="top_row path_label">Bottom Upgrade Path</th>
+                        <th className="top_row upgrade_button">EZ Collect</th>
+                        <th className="top_row upgrade_button">Banana Salvage</th>
+                        <th className="top_row upgrade_button">Marketplace</th>
+                        <th className="top_row upgrade_button">Central Market</th>
+                        <th className="top_row upgrade_button">Monkey Wall Street</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -133,11 +188,43 @@ export function BananaMain() {
                     </tr>
                     <tr>
                         <th>Changes</th>
-                        <td>To be added</td>
-                        <td>To be added</td>
-                        <td>To be added</td>
-                        <td>To be added</td>
-                        <td>To be added</td>
+                        <td class="change_cell">
+                            <ul class="change_list">
+                                <li><i>Player can collect bananas from 50% further away</i></li>
+                                <li><i>Spoiled bananas auto-collect for half their original value</i></li>
+                            </ul>
+                        </td>
+                        <td class="change_cell">
+                            <ul class="change_list">
+                                <li><i>Spoiled bananas auto collect for 85% of their original value</i></li>
+                                <li>Sellback rate of this Banana Farm increased by 10%</li>
+                            </ul>
+                        </td>
+                        <td class="change_cell">
+                            <ul class="change_list">
+                                <li><i>No longer spawns bananas, instead adding income automatically to user total</i></li>
+                                <li>Generates 16 bunches of $20 per round</li>
+                                <li>Income/round $80 -&gt; $320</li>
+                            </ul>
+                        </td>
+                        <td class="change_cell">
+                            <ul class="change_list">
+                                <li>Bunch value $20 -&gt; $70</li>
+                                <li><i>Merchantman buff: all xx3 Buccaneers and higher receive +10% income</i></li>
+                                <li><i>Merchantman buff stacks up to 10 times</i></li>
+                                <li>Income/round $320 -&gt; $1,120</li>
+                            </ul>
+                        </td>
+                        <td class="change_cell">
+                            <ul class="change_list">
+                                <li>+Generates $4,000 at end of each round (in addition to bunch generation from previous upgrades)</li>
+                                <li>+Generates 15 lives/round</li>
+                                <li>+35 range</li>
+                                <li><i>Auto-collect mechanic: Monkey Wall street auto-collects in-range bananas, full Bloon traps,
+                                     and Supply Drop crates</i></li>
+                                <li>Income/round $1,120 -&gt; $5,120</li>
+                            </ul>
+                        </td>
                     </tr>
                 </tbody>
             </table>
