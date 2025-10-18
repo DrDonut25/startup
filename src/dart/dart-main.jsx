be6444ce72747399b49770e78c0a0ddc73f8a0f5
DrDonut25 <remundparker@gmail.com>
2025-10-18 13:51:57 -0600

    Added styling class tags to dart/dartling; filled in changes table cells. Also added horizontal overflow styling so that background/header doesn't cut off at window width

--- src/dart/dart-main.jsx
+++ src/dart/dart-main.jsx
@@ -64,12 +64,12 @@ export function DartMain() {
             <table className="upgrade_table">
                 <thead>
                     <tr>
-                        <th className="top_row">Top Upgrade Path</th>
-                        <th className="top_row">Sharp Shots</th>
-                        <th className="top_row">Razor Sharp Shots</th>
-                        <th className="top_row">Spike-o-pult</th>
-                        <th className="top_row">Juggernaut</th>
-                        <th className="top_row">Ultra-Juggernaut</th>
+                        <th className="top_row path_label">Top Upgrade Path</th>
+                        <th className="top_row upgrade_button">Sharp Shots</th>
+                        <th className="top_row upgrade_button">Razor Sharp Shots</th>
+                        <th className="top_row upgrade_button">Spike-o-pult</th>
+                        <th className="top_row upgrade_button">Juggernaut</th>
+                        <th className="top_row upgrade_button last_upgrade_button">Ultra-Juggernaut</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -83,11 +83,47 @@ export function DartMain() {
                     </tr>
                     <tr>
                         <th>Changes</th>
-                        <td>To be added</td>
-                        <td>To be added</td>
-                        <td>To be added</td>
-                        <td>To be added</td>
-                        <td>To be added</td>
+                        <td class="change_cell">
+                            <ul class="change_list">
+                                <li>Pierce 2 -&gt; 3</li>
+                            </ul>
+                        </td>
+                        <td class="change_cell">
+                            <ul class="change_list">
+                                <li>Pierce 3 -&gt; 5</li>
+                            </ul>
+                        </td>
+                        <td class="change_cell">
+                            <ul class="change_list">
+                                <li>+4.8 range</li>
+                                <li>-17.4% attack speed</li>
+                                <li>Damage 1 -&gt; 2</li>
+                                <li>Pierce 5 -&gt; 18</li>
+                                <li>+Projectiles bounce off map obstacles</li>
+                                <li>+Can pop Frozen</li>
+                            </ul>
+                        </td>
+                        <td class="change_cell">
+                            <ul class="change_list">
+                                <li>+15% attack speed</li>
+                                <li>Doubled projectile speed</li>
+                                <li>Pierce 18 -&gt; 60</li>
+                                <li>+3 Ceramic damage</li>
+                                <li>+2 Fortified damage</li>
+                                <li>+Can pop Lead</li>
+                                <li>+Bloon knockback</li>
+                            </ul>
+                        </td>
+                        <td class="change_cell">
+                            <ul class="change_list">
+                                <li>Damage 2 -&gt; 5</li>
+                                <li>Fortified bonus damage 2 -&gt; 5</li>
+                                <li>Ceramic bonus damage 3 -&gt; 8</li>
+                                <li>+20 Lead damage</li>
+                                <li>Pierce 60 -&gt; 210</li>
+                                <li><i>+Splits into 6 juggernaut projectiles after main projectile expiration</i></li>
+                            </ul>
+                        </td>
                     </tr>
                 </tbody>
             </table>
@@ -95,12 +131,12 @@ export function DartMain() {
             <table className="upgrade_table">
                 <thead>
                     <tr>
-                        <th className="top_row">Middle Upgrade Path</th>
-                        <th className="top_row">Quick Shots</th>
-                        <th className="top_row">Very Quick Shots</th>
-                        <th className="top_row">Triple Shot</th>
-                        <th className="top_row">Super Monkey Fan Club</th>
-                        <th className="top_row">Plasma Monkey Fan Club</th>
+                        <th className="top_row path_label">Middle Upgrade Path</th>
+                        <th className="top_row upgrade_button">Quick Shots</th>
+                        <th className="top_row upgrade_button">Very Quick Shots</th>
+                        <th className="top_row upgrade_button">Triple Shot</th>
+                        <th className="top_row upgrade_button">Super Monkey Fan Club</th>
+                        <th className="top_row upgrade_button last_upgrade_button">Plasma Monkey Fan Club</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -114,11 +150,43 @@ export function DartMain() {
                     </tr>
                     <tr>
                         <th>Changes</th>
-                        <td>To be added</td>
-                        <td>To be added</td>
-                        <td>To be added</td>
-                        <td>To be added</td>
-                        <td>To be added</td>
+                        <td class="change_cell">
+                            <ul class="change_list">
+                                <li>+15% attack speed</li>
+                            </ul>
+                        </td>
+                        <td class="change_cell">
+                            <ul class="change_list">
+                                <li>+18% attack speed</li>
+                            </ul>
+                        </td>
+                        <td class="change_cell">
+                            <ul class="change_list">
+                                <li>+25% attack speed</li>
+                                <li>Now throws 3 darts/attack in 30° cone</li>
+                            </ul>
+                        </td>
+                        <td class="change_cell">
+                            <ul class="change_list">
+                                <li>+100% attack speed</li>
+                                <li><span class="ability">Ability: transform 10 nearby Dart Monkeys (including this one) into Super
+                                     Monkeys for 15s</span></li>
+                                <li><span class="ability">Transformed Dart Monkey stats: 2 pierce, 0.06 attack cooldown,
+                                     40 base range</span></li>
+                                <li><span class="ability">Transformed Dart Monkeys inherit crosspath stats
+                                     (not crosspath stats from this tower)</span></li>
+                                <li><span class="ability">Ability cooldown: 50s</span></li>
+                            </ul>
+                        </td>
+                        <td class="change_cell">
+                            <ul class="change_list">
+                                <li><span class="ability">Ability: transform 20 nearby Dart Monkeys into Plasma Monkeys for 15s</span></li>
+                                <li><span class="ability">Transformed Dart Monkey Stats: 5 pierce, 0.03 attack cooldown, 2 damage</span></li>
+                                <li><span class="ability">Transformed Dart Monkeys inherit crosspath stats
+                                     (not crosspath stats from this tower)</span></li>
+                                <li><span class="ability">Ability cooldown: 50s</span></li>
+                            </ul>
+                        </td>
                     </tr>
                 </tbody>
             </table>
@@ -126,12 +194,12 @@ export function DartMain() {
             <table className="upgrade_table">
                 <thead>
                     <tr>
-                        <th className="top_row">Bottom Upgrade Path</th>
-                        <th className="top_row">Long Range Darts</th>
-                        <th className="top_row">Enhanced Eyesight</th>
-                        <th className="top_row">Crossbow</th>
-                        <th className="top_row">Sharpshooter</th>
-                        <th className="top_row">Crossbow Master</th>
+                        <th className="top_row path_label">Bottom Upgrade Path</th>
+                        <th className="top_row upgrade_button">Long Range Darts</th>
+                        <th className="top_row upgrade_button">Enhanced Eyesight</th>
+                        <th className="top_row upgrade_button">Crossbow</th>
+                        <th className="top_row upgrade_button">Sharpshooter</th>
+                        <th className="top_row upgrade_button last_upgrade_button">Crossbow Master</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -145,11 +213,45 @@ export function DartMain() {
                     </tr>
                     <tr>
                         <th>Changes</th>
-                        <td>To be added</td>
-                        <td>To be added</td>
-                        <td>To be added</td>
-                        <td>To be added</td>
-                        <td>To be added</td>
+                        <td class="change_cell">
+                            <ul class="change_list">
+                                <li>+8 range</li>
+                                <li>Enhanced projectile lifespan</li>
+                            </ul>
+                        </td>
+                        <td class="change_cell">
+                            <ul class="change_list">
+                                <li>+8 range</li>
+                                <li>Further enhanced projectile lifespan</li>
+                                <li>+Faster projectile speed</li>
+                                <li>+Camo detection</li>
+                            </ul>
+                        </td>
+                        <td class="change_cell">
+                            <ul class="change_list">
+                                <li>+12 range</li>
+                                <li>Damage 1 -&gt; 3</li>
+                                <li>Pierce 2 -&gt; 4</li>
+                                <li>+Faster projectile speed</li>
+                            </ul>
+                        </td>
+                        <td class="change_cell">
+                            <ul class="change_list">
+                                <li>Damage 3 -&gt; 6</li>
+                                <li>+100% attack speed</li>
+                                <li>+Faster projectile speed</li>
+                                <li><i>Critical Hit mechanic: every 10th attack deals 50 damage instead of 6</i></li>
+                            </ul>
+                        </td>
+                        <td class="change_cell">
+                            <ul class="change_list">
+                                <li>Damage 6 -&gt; 8</li>
+                                <li>+100% attack speed</li>
+                                <li>Pierce 4 -&gt; 8</li>
+                                <li><i>Critical hit damage 50 -&gt; 80; crits every 5 hits</i></li>
+                                <li>+Can pop Lead</li>
+                            </ul>
+                        </td>
                     </tr>
                 </tbody>
             </table>
--- src/dartling/dartling-main.jsx
+++ src/dartling/dartling-main.jsx
@@ -64,12 +64,12 @@ export function DartlingMain() {
             <table className="upgrade_table">
                 <thead>
                     <tr>
-                        <th className="top_row">Top Upgrade Path</th>
-                        <th className="top_row">Focused Firing</th>
-                        <th className="top_row">Laser Shock</th>
-                        <th className="top_row">Laser Cannon</th>
-                        <th className="top_row">Plasma Accelerator</th>
-                        <th className="top_row">Ray of Doom</th>
+                        <th className="top_row path_label">Top Upgrade Path</th>
+                        <th className="top_row upgrade_button">Focused Firing</th>
+                        <th className="top_row upgrade_button">Laser Shock</th>
+                        <th className="top_row upgrade_button">Laser Cannon</th>
+                        <th className="top_row upgrade_button">Plasma Accelerator</th>
+                        <th className="top_row upgrade_button last_upgrade_button">Ray of Doom</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -83,11 +83,48 @@ export function DartlingMain() {
                     </tr>
                     <tr>
                         <th>Changes</th>
-                        <td>To be added</td>
-                        <td>To be added</td>
-                        <td>To be added</td>
-                        <td>To be added</td>
-                        <td>To be added</td>
+                        <td class="change_cell">
+                            <ul class="change_list">
+                                <li>Projectile spread 23° -&gt; 9.2°</li>
+                                <li>Increased projectile lifespan</li>
+                            </ul>
+                        </td>
+                        <td class="change_cell">
+                            <ul class="change_list">
+                                <li><i>+Hit Bloons become shocked for 1s: takes +1 from all other 2xx Dartling Gunners—shocked Bloons
+                                     take 1 damage/s for 1s</i></li>
+                            </ul>
+                        </td>
+                        <td class="change_cell">
+                            <ul class="change_list">
+                                <li>Damage 1 -&gt; 2</li>
+                                <li>Pierce 1 -&gt; 6</li>
+                                <li>+~43% projectile speed</li>
+                                <li>+2 MOAB damage</li>
+                                <li><i>Shock duration 1s -&gt; 2s</i></li>
+                                <li>Shots can pop Frozen, but not Lead/Purple</li>
+                            </ul>
+                        </td>
+                        <td class="change_cell">
+                            <ul class="change_list">
+                                <li>Lasers replaced w/ continuous plasma beam that extends to selected target point: beam deals
+                                     frequent damage, but tip of beam even more powerful </li>
+                                <li>Beam stats: 1 damage, 50 pierce, 0.2s attack cooldown</li>
+                                <li>Beam Tip stats: 2 damage, +10 MOAB damage, 50 pierce, 0.2s attack cooldown</li>
+                                <li>+Can pop Lead</li>
+                            </ul>
+                        </td>
+                        <td class="change_cell">
+                            <ul class="change_list">
+                                <li>Laser beam is larger and extends infinitely rather than to selected target point</li>
+                                <li>Damage 1 -&gt; 30</li>
+                                <li>Pierce 50 -&gt; 999</li>
+                                <li>Shocked Bloon bonus damage 1 -&gt; 20</li>
+                                <li>Shock now deals 20 damage/s rather than 1 damage/s</li>
+                                <li>First hit from laser deals +110 damage</li>
+                                <li>+Can pop Purple (now pops all Bloon types except Camo)</li>
+                            </ul>
+                        </td>
                     </tr>
                 </tbody>
             </table>
@@ -95,12 +132,12 @@ export function DartlingMain() {
             <table className="upgrade_table">
                 <thead>
                     <tr>
-                        <th className="top_row">Middle Upgrade Path</th>
-                        <th className="top_row">Advanced Targeting</th>
-                        <th className="top_row">Faster Barrel Spin</th>
-                        <th className="top_row">Hydra Rocket Pods</th>
-                        <th className="top_row">Rocket Storm</th>
-                        <th className="top_row">M.A.D.</th>
+                        <th className="top_row path_label">Middle Upgrade Path</th>
+                        <th className="top_row upgrade_button">Advanced Targeting</th>
+                        <th className="top_row upgrade_button">Faster Barrel Spin</th>
+                        <th className="top_row upgrade_button">Hydra Rocket Pods</th>
+                        <th className="top_row upgrade_button">Rocket Storm</th>
+                        <th className="top_row upgrade_button last_upgrade_button">M.A.D.</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -114,11 +151,44 @@ export function DartlingMain() {
                     </tr>
                     <tr>
                         <th>Changes</th>
-                        <td>To be added</td>
-                        <td>To be added</td>
-                        <td>To be added</td>
-                        <td>To be added</td>
-                        <td>To be added</td>
+                        <td class="change_cell">
+                            <ul class="change_list">
+                                <li>+Camo detection</li>
+                            </ul>
+                        </td>
+                        <td class="change_cell">
+                            <ul class="change_list">
+                                <li>Attack cooldown 0.2s -&gt; 0.132s</li>
+                            </ul>
+                        </td>
+                        <td class="change_cell">
+                            <ul class="change_list">
+                                <li>Now fires missiles that explode up to 3 times: 2 explosion damage, 6 pierce, 8-unit blast radius</li>
+                                <li>(Explosion count can be increased by external pierce buffs)</li>
+                                <li>+Can pop ALL Bloon types</li>
+                            </ul>
+                        </td>
+                        <td class="change_cell">
+                            <ul class="change_list">
+                                <li>Explosion count 3 -&gt; 4</li>
+                                <li>Explosion pierce 6 -&gt; 8</li>
+                                <li><span class="ability">Ability: repeatedly fire waves of 10 missiles in 90° cone every 0.5s for 7.5s:
+                                     5 damage, +1 Ceramic damage, 8 explosion pierce, 4 explosions per missile</span></li>
+                                <li><span class="ability">Ability cooldown: 40s</span></li>
+                            </ul>
+                        </td>
+                        <td class="change_cell">
+                            <ul class="change_list">
+                                <li>Main attacks now fire mega missiles that fire 3x more slowly, but deal much more damage to MOABs
+                                     (see below)</li>
+                                <li>Explosion count 4 -&gt; 10</li>
+                                <li>Explosion damage 2 -&gt; 3</li>
+                                <li>MOABs hit directly by mega missiles (not their explosions) take +450 damage</li>
+                                <li><span class="ability">Ability missile explosion count 4 -&gt; 9</span></li>
+                                <li><span class="ability">Ability missile bonus Ceramic damage 1 -&gt; 5</span></li>
+                                <li><span class="ability">+250 MOAB damage (ability missiles)</span></li>
+                            </ul>
+                        </td>
                     </tr>
                 </tbody>
             </table>
@@ -126,12 +196,12 @@ export function DartlingMain() {
             <table className="upgrade_table">
                 <thead>
                     <tr>
-                        <th className="top_row">Bottom Upgrade Path</th>
-                        <th className="top_row">Faster Swivel</th>
-                        <th className="top_row">Powerful Darts</th>
-                        <th className="top_row">Buckshot</th>
-                        <th className="top_row">Bloon Area Denial System</th>
-                        <th className="top_row">Bloon Exclusion Zone</th>
+                        <th className="top_row path_label">Bottom Upgrade Path</th>
+                        <th className="top_row upgrade_button">Faster Swivel</th>
+                        <th className="top_row upgrade_button">Powerful Darts</th>
+                        <th className="top_row upgrade_button">Buckshot</th>
+                        <th className="top_row upgrade_button">Bloon Area Denial System</th>
+                        <th className="top_row upgrade_button last_upgrade_button">Bloon Exclusion Zone</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -145,11 +215,42 @@ export function DartlingMain() {
                     </tr>
                     <tr>
                         <th>Changes</th>
-                        <td>To be added</td>
-                        <td>To be added</td>
-                        <td>To be added</td>
-                        <td>To be added</td>
-                        <td>To be added</td>
+                        <td class="change_cell">
+                            <ul class="change_list">
+                                <li>Swivel speed 180°/s -&gt; 440°/s</li>
+                            </ul>
+                        </td>
+                        <td class="change_cell">
+                            <ul class="change_list">
+                                <li>Pierce 1 -&gt; 3</li>
+                                <li>+~34% dart speed</li>
+                                <li>+Can pop Frozen</li>
+                            </ul>
+                        </td>
+                        <td class="change_cell">
+                            <ul class="change_list">
+                                <li>Now fires 6 buckshots/attack—buckshots are faster and knock back non-MOAB Bloons</li>
+                                <li>Buckshot stats: 4 damage, 4 pierce, 46° spread, 1.5s attack cooldown</li>
+                            </ul>
+                        </td>
+                        <td class="change_cell">
+                            <ul class="change_list">
+                                <li>Attack cooldown 1.5s -&gt; 0.375s</li>
+                                <li><i>+Target Independent targeting option: when selected, 4 barrels will target First/Last/Close/Strong
+                                     Bloons all at once (1 target priority per barrel)</i></li>
+                                <li><i>Target Independent range: 130 units</i></li>
+                            </ul>
+                        </td>
+                        <td class="change_cell">
+                            <ul class="change_list">
+                                <li>Attack cooldown 0.375s -&gt; 0.25s</li>
+                                <li>Pierce 4 -&gt; 6</li>
+                                <li>Damage 4 -&gt; 8</li>
+                                <li>Buckshot count per shot 6 -&gt; 12</li>
+                                <li><i>Target Independent now has 6 barrels: 2 will target First, 2 will target Strong, 1 will target
+                                     Last, 1 will target Close</i></li>
+                            </ul>
+                        </td>
                     </tr>
                 </tbody>
             </table>
