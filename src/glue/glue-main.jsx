77a5205b8f041a597f37685d70e88bd786d5e319
DrDonut25 <remundparker@gmail.com>
2025-10-18 20:18:24 -0600

    Added styling class tags to engineer/glue; filled in changes table cells.

--- src/glue/glue-main.jsx
+++ src/glue/glue-main.jsx
@@ -76,12 +76,12 @@ export function GlueMain() {
             <table className="upgrade_table">
                 <thead>
                     <tr>
-                        <th className="top_row">Top Upgrade Path</th>
-                        <th className="top_row">Glue Soak</th>
-                        <th className="top_row">Corrosive Glue</th>
-                        <th className="top_row">Bloon Dissolver</th>
-                        <th className="top_row">Bloon Liquefier</th>
-                        <th className="top_row">Bloon Solver</th>
+                        <th className="top_row path_label">Top Upgrade Path</th>
+                        <th className="top_row upgrade_button">Glue Soak</th>
+                        <th className="top_row upgrade_button">Corrosive Glue</th>
+                        <th className="top_row upgrade_button">Bloon Dissolver</th>
+                        <th className="top_row upgrade_button">Bloon Liquefier</th>
+                        <th className="top_row upgrade_button last_upgrade_button">Bloon Solver</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -95,11 +95,43 @@ export function GlueMain() {
                     </tr>
                     <tr>
                         <th>Changes</th>
-                        <td>To be added</td>
-                        <td>To be added</td>
-                        <td>To be added</td>
-                        <td>To be added</td>
-                        <td>To be added</td>
+                        <td class="change_cell">
+                            <ul class="change_list">
+                                <li>Layers glued/glob 3 -&gt; Infinite</li>
+                            </ul>
+                        </td>
+                        <td class="change_cell">
+                            <ul class="change_list">
+                                <li><i>+Damage over time effect: Glued Bloons take 1 damage every 2 seconds</i></li>
+                                <li>+Glue can damage MOABs (but cannot slow them)</li>
+                            </ul>
+                        </td>
+                        <td class="change_cell">
+                            <ul class="change_list">
+                                <li>Damage over time increased to 2 damage/s</li>
+                                <li>+2 Ceramic damage/s</li>
+                                <li>Pierce 1 -&gt; 2</li>
+                                <li>Attack cooldown 1s -&gt; 0.5s</li>
+                            </ul>
+                        </td>
+                        <td class="change_cell">
+                            <ul class="change_list">
+                                <li>Damage over time increased to 10 damage/s</li>
+                                <li><i>Acid puddle mechanic: when Bloon glued by this tower pops, it drops puddle of acid that lingers
+                                     on track for 7.7s. Each puddle deals 4 damage to up to 3 Bloons</i></li>
+                            </ul>
+                        </td>
+                        <td class="change_cell">
+                            <ul class="change_list">
+                                <li>Ceramic bonus 2 damage/s -&gt; 80 damage/s</li>
+                                <li>+40 MOAB damage/s</li>
+                                <li>Acid puddle damage 4 -&gt; 15</li>
+                                <li>Pierce 2 -&gt; 4</li>
+                                <li>Now shoots 2 globs of glue per attack</li>
+                                <li>Attack cooldown 0.5s -&gt; 0.25s</li>
+                                <li>+Splatter glue can hit 4 additional Bloons per shot</li>
+                            </ul>
+                        </td>
                     </tr>
                 </tbody>
             </table>
@@ -107,12 +139,12 @@ export function GlueMain() {
             <table className="upgrade_table">
                 <thead>
                     <tr>
-                        <th className="top_row">Middle Upgrade Path</th>
-                        <th className="top_row">Bigger Globs</th>
-                        <th className="top_row">Glue Splatter</th>
-                        <th className="top_row">Glue Hose</th>
-                        <th className="top_row">Glue Strike</th>
-                        <th className="top_row">Glue Storm</th>
+                        <th className="top_row path_label">Middle Upgrade Path</th>
+                        <th className="top_row upgrade_button">Bigger Globs</th>
+                        <th className="top_row upgrade_button">Glue Splatter</th>
+                        <th className="top_row upgrade_button">Glue Hose</th>
+                        <th className="top_row upgrade_button">Glue Strike</th>
+                        <th className="top_row upgrade_button last_upgrade_button">Glue Storm</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -126,11 +158,36 @@ export function GlueMain() {
                     </tr>
                     <tr>
                         <th>Changes</th>
-                        <td>To be added</td>
-                        <td>To be added</td>
-                        <td>To be added</td>
-                        <td>To be added</td>
-                        <td>To be added</td>
+                        <td class="change_cell">
+                            <ul class="change_list">
+                                <li>Pierce 1 -&gt; 2</li>
+                            </ul>
+                        </td>
+                        <td class="change_cell">
+                            <ul class="change_list">
+                                <li>+Splatter glue can hit 4 additional Bloons per shot</li>
+                            </ul>
+                        </td>
+                        <td class="change_cell">
+                            <ul class="change_list">
+                                <li>Attack cooldown  1s -&gt; 0.34s</li>
+                                <li>+12 range</li>
+                            </ul>
+                        </td>
+                        <td class="change_cell">
+                            <ul class="change_list">
+                                <li><span class="ability">Ability: glue all Bloons (including MOAB/Camo) on screen for 11s</span></li>
+                                <li><span class="ability">Ability cooldown: 30s</span></li>
+                                <li><span class="ability">+Bloons glued by ability take +2 damage & temporarily lose Lead/Frozen properties</span></li>
+                            </ul>
+                        </td>
+                        <td class="change_cell">
+                            <ul class="change_list">
+                                <li><span class="ability">Ability now glues all Bloons on screen every second for 10s (glue duration
+                                     is still 11s)</span></li>
+                                <li><span class="ability">Ability cooldown 30s -&gt; 40s</span></li>
+                            </ul>
+                        </td>
                     </tr>
                 </tbody>
             </table>
@@ -138,12 +195,12 @@ export function GlueMain() {
             <table className="upgrade_table">
                 <thead>
                     <tr>
-                        <th className="top_row">Bottom Upgrade Path</th>
-                        <th className="top_row">Stickier Glue</th>
-                        <th className="top_row">Stronger Glue</th>
-                        <th className="top_row">MOAB Glue</th>
-                        <th className="top_row">Relentless Glue</th>
-                        <th className="top_row">Super Glue</th>
+                        <th className="top_row path_label">Bottom Upgrade Path</th>
+                        <th className="top_row upgrade_button">Stickier Glue</th>
+                        <th className="top_row upgrade_button">Stronger Glue</th>
+                        <th className="top_row upgrade_button">MOAB Glue</th>
+                        <th className="top_row upgrade_button">Relentless Glue</th>
+                        <th className="top_row upgrade_button last_upgrade_button">Super Glue</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -157,11 +214,39 @@ export function GlueMain() {
                     </tr>
                     <tr>
                         <th>Changes</th>
-                        <td>To be added</td>
-                        <td>To be added</td>
-                        <td>To be added</td>
-                        <td>To be added</td>
-                        <td>To be added</td>
+                        <td class="change_cell">
+                            <ul class="change_list">
+                                <li>Glue duration 11s -&gt; 24s</li>
+                            </ul>
+                        </td>
+                        <td class="change_cell">
+                            <ul class="change_list">
+                                <li>Glue slow intensity -50% -&gt; -75% Bloon speed</li>
+                            </ul>
+                        </td>
+                        <td class="change_cell">
+                            <ul class="change_list">
+                                <li>+Can now glue/slow MOABs for 9s</li>
+                                <li>MOAB glue slow intensity: -37.5% MOAB speed</li>
+                            </ul>
+                        </td>
+                        <td class="change_cell">
+                            <ul class="change_list">
+                                <li><i>+Glue Stun mechanic: Glued Bloons release splatter that stun up to 6 nearby Bloons when popped</i></li>
+                                <li><i>Stun duration: 1s (non-MOAB-class), 0.25s (Blue MOABs/DDTs) </i></li>
+                                <li>Glue on MOAB duration 9s -&gt; 12s</li>
+                            </ul>
+                        </td>
+                        <td class="change_cell">
+                            <ul class="change_list">
+                                <li>Glue slow intensity -75% -&gt; -100% Bloon speed (aka Glued Bloons and MOABs are completely immobilized)</li>
+                                <li>Glue projectiles now deal 30 damage to MOABs and 1 damage to non-MOABs on contact</li>
+                                <li>Glue glob pierce 1 -&gt; 6</li>
+                                <li>Stunning splat pierce 6 -&gt; 11</li>
+                                <li>Splat now stuns BFBs and ZOMGs, and all MOABs now stunned for 1s</li>
+                                <li>Glue on MOAB duration 12s -&gt; 18s</li>
+                            </ul>
+                        </td>
                     </tr>
                 </tbody>
             </table>
