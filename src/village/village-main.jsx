73b8682e3ff92cf33b2ed5a13b5c63659aecce03
DrDonut25 <remundparker@gmail.com>
2025-10-19 18:08:26 -0600

    Added styling class tags to village/wizard; filled in changes table cells.

--- src/village/village-main.jsx
+++ src/village/village-main.jsx
@@ -44,12 +44,12 @@ export function VillageMain() {
             <table className="upgrade_table">
                 <thead>
                     <tr>
-                        <th className="top_row">Top Upgrade Path</th>
-                        <th className="top_row">Bigger Radius</th>
-                        <th className="top_row">Jungle Drums</th>
-                        <th className="top_row">Primary Mentoring</th>
-                        <th className="top_row">Primary Academy</th>
-                        <th className="top_row">Primary Expertise</th>
+                        <th className="top_row path_label">Top Upgrade Path</th>
+                        <th className="top_row upgrade_button">Bigger Radius</th>
+                        <th className="top_row upgrade_button">Jungle Drums</th>
+                        <th className="top_row upgrade_button">Primary Mentoring</th>
+                        <th className="top_row upgrade_button">Primary Academy</th>
+                        <th className="top_row upgrade_button last_upgrade_button">Primary Expertise</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -63,11 +63,40 @@ export function VillageMain() {
                     </tr>
                     <tr>
                         <th>Changes</th>
-                        <td>To be added</td>
-                        <td>To be added</td>
-                        <td>To be added</td>
-                        <td>To be added</td>
-                        <td>To be added</td>
+                        <td className="change_cell">
+                            <ul className="change_list">
+                                <li>+8 range</li>
+                            </ul>
+                        </td>
+                        <td className="change_cell">
+                            <ul className="change_list">
+                                <li><i>+Jungle Drums buff: in-range towers receive -15% attack cooldown</i></li>
+                            </ul>
+                        </td>
+                        <td className="change_cell">
+                            <ul className="change_list">
+                                <li><i>+Primary Training buff: in-range Primary Monkeys receive +10% range, +1 pierce, and +25%
+                                     projectile speed </i></li>
+                            </ul>
+                        </td>
+                        <td className="change_cell">
+                            <ul className="change_list">
+                                <li><i>+Primary Mentoring buff: in-range Primary Monkeys receive +5 range, -15% ability cooldowns, and
+                                     receive all Tier 1 upgrades for free</i></li>
+                                <li>+7 range</li>
+                            </ul>
+                        </td>
+                        <td className="change_cell">
+                            <ul className="change_list">
+                                <li><i>Primary Monkey pierce buff +1 -&gt; +3</i></li>
+                                <li><i>Primary Monkey ability cooldown buff -15% -&gt; 25%</i></li>
+                                <li><i>+In-range Primary Monkeys now also receive all Tier 2 upgrades for free</i></li>
+                                <li>+Mega Ballista attack: fires infinite-range ballista projectiles that bounce Glaive Ricochet-style
+                                     between Bloons</li>
+                                <li>Mega Ballista stats: 10 damage, +270 Ceramic/MOAB damage, 100 pierce (though MOABs take up 10 pierce
+                                     each), 3s attack cooldown, can pop ALL Bloon types (including Camo)</li>
+                            </ul>
+                        </td>
                     </tr>
                 </tbody>
             </table>
@@ -75,12 +104,12 @@ export function VillageMain() {
             <table className="upgrade_table">
                 <thead>
                     <tr>
-                        <th className="top_row">Middle Upgrade Path</th>
-                        <th className="top_row">Regrow Blocker</th>
-                        <th className="top_row">Radar Scanner</th>
-                        <th className="top_row">Monkey Intelligence Bureau</th>
-                        <th className="top_row">Call to Arms</th>
-                        <th className="top_row">Homeland Defense</th>
+                        <th className="top_row path_label">Middle Upgrade Path</th>
+                        <th className="top_row upgrade_button">Regrow Blocker</th>
+                        <th className="top_row upgrade_button">Radar Scanner</th>
+                        <th className="top_row upgrade_button">Monkey Intelligence Bureau</th>
+                        <th className="top_row upgrade_button">Call to Arms</th>
+                        <th className="top_row upgrade_button last_upgrade_button">Homeland Defense</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -94,11 +123,33 @@ export function VillageMain() {
                     </tr>
                     <tr>
                         <th>Changes</th>
-                        <td>To be added</td>
-                        <td>To be added</td>
-                        <td>To be added</td>
-                        <td>To be added</td>
-                        <td>To be added</td>
+                        <td className="change_cell">
+                            <ul className="change_list">
+                                <li><i>+Regrow Bloons cannot regrow any layers while in range</i></li>
+                            </ul>
+                        </td>
+                        <td className="change_cell">
+                            <ul className="change_list">
+                                <li><i>+Radar Scanner buff: in-range towers receive Camo detection</i></li>
+                            </ul>
+                        </td>
+                        <td className="change_cell">
+                            <ul className="change_list">
+                                <li><i>+MIB buff: in-range towers can now pop all Bloon types</i></li>
+                            </ul>
+                        </td>
+                        <td className="change_cell">
+                            <ul className="change_list">
+                                <li><span className="ability">Ability: all towers on screen receive +50% attack speed and +50% pierce
+                                     for 15s</span></li>
+                                <li><span className="ability">Ability cooldown: 45s</span></li>
+                            </ul>
+                        </td>
+                        <td className="change_cell">
+                            <ul className="change_list">
+                                <li><span className="ability">Ability now grants +100% attack speed/pierce for 20s</span></li>
+                            </ul>
+                        </td>
                     </tr>
                 </tbody>
             </table>
@@ -106,12 +157,12 @@ export function VillageMain() {
             <table className="upgrade_table">
                 <thead>
                     <tr>
-                        <th className="top_row">Bottom Upgrade Path</th>
-                        <th className="top_row">Monkey Business</th>
-                        <th className="top_row">Monkey Commerce</th>
-                        <th className="top_row">Monkey Town</th>
-                        <th className="top_row">Monkey City</th>
-                        <th className="top_row">Monkeyopolis</th>
+                        <th className="top_row path_label">Bottom Upgrade Path</th>
+                        <th className="top_row upgrade_button">Monkey Business</th>
+                        <th className="top_row upgrade_button">Monkey Commerce</th>
+                        <th className="top_row upgrade_button">Monkey Town</th>
+                        <th className="top_row upgrade_button">Monkey City</th>
+                        <th className="top_row upgrade_button last_upgrade_button">Monkeyopolis</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -125,11 +176,38 @@ export function VillageMain() {
                     </tr>
                     <tr>
                         <th>Changes</th>
-                        <td>To be added</td>
-                        <td>To be added</td>
-                        <td>To be added</td>
-                        <td>To be added</td>
-                        <td>To be added</td>
+                        <td className="change_cell">
+                            <ul className="change_list">
+                                <li><i>+Monkey Business buff: in-range towers receive 10% discount on all Tier-3-or-lower upgrades</i></li>
+                            </ul>
+                        </td>
+                        <td className="change_cell">
+                            <ul className="change_list">
+                                <li><i>+Monkey Commerce buff: in-range towers receive 5% discount on all Tier-3-or-lower upgrades</i></li>
+                                <li><i>This buff stacks up to 3 times (and once with Monkey Business buff)</i></li>
+                            </ul>
+                        </td>
+                        <td className="change_cell">
+                            <ul className="change_list">
+                                <li><i>+Monkey Town buff: in-range towers receive +50% cash per pop</i></li>
+                            </ul>
+                        </td>
+                        <td className="change_cell">
+                            <ul className="change_list">
+                                <li><i>+Monkey City buff: in-range towers that generate income (e.g. Banana Farms, Jungle's Bounty)
+                                     generate 20% more income </i></li>
+                                <li><i>+Player receives 1 free Dart Monkey each round</i></li>
+                                <li>+10 range</li>
+                            </ul>
+                        </td>
+                        <td className="change_cell">
+                            <ul className="change_list">
+                                <li>Absorbs all in-range Tier-4-or-lower Banana Farms, then begins producing 10 banana crates each
+                                     round—crate value dependent on total cost of absorbed Banana Farms</li>
+                                <li>Minimum cash generated/round (e.g. if 1 unupgraded Banana Farm is absorbed): $1,200</li>
+                                <li>Monkeyopolis crates worth $200 more for every $2,000 spent on absorbed Banana Farms</li>
+                            </ul>
+                        </td>
                     </tr>
                 </tbody>
             </table>
--- src/wizard/wizard-main.jsx
+++ src/wizard/wizard-main.jsx
@@ -64,12 +64,12 @@ export function WizardMain() {
             <table className="upgrade_table">
                 <thead>
                     <tr>
-                        <th className="top_row">Top Upgrade Path</th>
-                        <th className="top_row">Guided Magic</th>
-                        <th className="top_row">Arcane Blast</th>
-                        <th className="top_row">Arcane Mastery</th>
-                        <th className="top_row">Arcane Spikes</th>
-                        <th className="top_row">Archmage</th>
+                        <th className="top_row path_label">Top Upgrade Path</th>
+                        <th className="top_row upgrade_button">Guided Magic</th>
+                        <th className="top_row upgrade_button">Arcane Blast</th>
+                        <th className="top_row upgrade_button">Arcane Mastery</th>
+                        <th className="top_row upgrade_button">Arcane Spikes</th>
+                        <th className="top_row upgrade_button last_upgrade_button">Archmage</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -83,11 +83,47 @@ export function WizardMain() {
                     </tr>
                     <tr>
                         <th>Changes</th>
-                        <td>To be added</td>
-                        <td>To be added</td>
-                        <td>To be added</td>
-                        <td>To be added</td>
-                        <td>To be added</td>
+                        <td className="change_cell">
+                            <ul className="change_list">
+                                <li>+Projectiles now home in on Bloons</li>
+                                <li>+Wizard now ignores map obstacles</li>
+                                <li>Doubled projectile lifespan</li>
+                            </ul>
+                        </td>
+                        <td className="change_cell">
+                            <ul className="change_list">
+                                <li>Damage 1 -&gt; 2</li>
+                                <li>+50% projectile size</li>
+                            </ul>
+                        </td>
+                        <td className="change_cell">
+                            <ul className="change_list">
+                                <li>+20 range</li>
+                                <li>Attack cooldown 1.1s -&gt; 0.55s</li>
+                                <li>Pierce 3 -&gt; 7</li>
+                                <li>Damage 2 -&gt; 3</li>
+                                <li>+75% projectile lifespan</li>
+                            </ul>
+                        </td>
+                        <td className="change_cell">
+                            <ul className="change_list">
+                                <li>Attack cooldown 0.55s -&gt; 0.275s</li>
+                                <li>Damage 3 -&gt; 6</li>
+                                <li>+10 MOAB damage</li>
+                                <li>+Can pop Lead</li>
+                            </ul>
+                        </td>
+                        <td className="change_cell">
+                            <ul className="change_list">
+                                <li>Attack cooldown 0.275s -&gt; 0.1375s</li>
+                                <li>Damage 6 -&gt; 8</li>
+                                <li>MOAB bonus damage 10 -&gt; 19</li>
+                                <li>Pierce 7 -&gt; 11</li>
+                                <li><i>+Dragon's Breath attack: 2 damage, +8 MOAB damage, 3 pierce, 0.0675s attack cooldown</i></li>
+                                <li><i>Flames set Bloons on fire, dealing 1 damage every 1.5s for 3s</i></li>
+                                <li><i>+Shimmer attack: Archmage removes camo from up to 500 Bloons in 75-unit radius every second</i></li>
+                            </ul>
+                        </td>
                     </tr>
                 </tbody>
             </table>
@@ -95,12 +131,12 @@ export function WizardMain() {
             <table className="upgrade_table">
                 <thead>
                     <tr>
-                        <th className="top_row">Middle Upgrade Path</th>
-                        <th className="top_row">Fireball</th>
-                        <th className="top_row">Wall of Fire</th>
-                        <th className="top_row">Dragon's Breath</th>
-                        <th className="top_row">Summon Phoenix</th>
-                        <th className="top_row">Wizard Lord Phoenix</th>
+                        <th className="top_row path_label">Middle Upgrade Path</th>
+                        <th className="top_row upgrade_button">Fireball</th>
+                        <th className="top_row upgrade_button">Wall of Fire</th>
+                        <th className="top_row upgrade_button">Dragon's Breath</th>
+                        <th className="top_row upgrade_button">Summon Phoenix</th>
+                        <th className="top_row upgrade_button last_upgrade_button">Wizard Lord Phoenix</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -114,11 +150,59 @@ export function WizardMain() {
                     </tr>
                     <tr>
                         <th>Changes</th>
-                        <td>To be added</td>
-                        <td>To be added</td>
-                        <td>To be added</td>
-                        <td>To be added</td>
-                        <td>To be added</td>
+                        <td className="change_cell">
+                            <ul className="change_list">
+                                <li>+Fireball attack: fireball deals 1 damage to 1 Bloon, and explosion deals additional 1 damage to up
+                                     to 15 Bloons</li>
+                                <li>Fireball attack cooldown: 2.2s</li>
+                                <li>+Fireballs pop Lead</li>
+                            </ul>
+                        </td>
+                        <td className="change_cell">
+                            <ul className="change_list">
+                                <li>+Firewall attack: places wall of fire on track every 6.5s—wall of fire lasts 4.5s</li>
+                                <li>Wall of fire stats: 1 damage every 0.15s, 10 pierce</li>
+                            </ul>
+                        </td>
+                        <td className="change_cell">
+                            <ul className="change_list">
+                                <li>+Dragon's Breath attack: 2 damage, 3 pierce, 0.135s attack cooldown</li>
+                                <li>Dragon's Breath attacks set Bloons on fire, dealing 1 damage every 1.5s for 3s</li>
+                                <li>Now fires 3 fireballs/attack in 60° cone</li>
+                                <li>Fireball explosion damage 1 -&gt; 3</li>
+                                <li>Wall of fire attack cooldown 6.5s -&gt; 4.5s</li>
+                                <li>Wall of fire damage cooldown 0.15s -&gt; 0.1s</li>
+                                <li>Wall of fire pierce 10 -&gt; 20</li>
+                            </ul>
+                        </td>
+                        <td className="change_cell">
+                            <ul className="change_list">
+                                <li><span className="ability">Ability: summon Phoenix that attacks Bloons anywhere on the map without
+                                     regard to map obstacles for 20s</span></li>
+                                <li><span className="ability">Phoenix stats: 5 damage, 8 pierce, 0.1s attack cooldown</span></li>
+                                <li><span className="ability">Ability cooldown: 45s</span></li>
+                                <li>Now fires 2 Dragon's Breath flame projectiles/attack in 10° cone</li>
+                            </ul>
+                        </td>
+                        <td className="change_cell">
+                            <ul className="change_list">
+                                <li><i>+Phoenix subtower becomes permanent</i></li>
+                                <li><span className="ability">Lava Phoenix Ability: transform Wizard Monkey into lava phoenix that
+                                     attacks Bloons with both flame and meteor attacks for 20s</span></li>
+                                <li><span className="ability">Lava phoenix flame attack stats: 20 damage, 50 pierce, 0.1s attack
+                                     cooldown</span></li>
+                                <li><span className="ability">Lava phoenix meteor attacks (fires 8 meteors in all directions):
+                                     50 damage, 300 pierce, 1s attack cooldown</span></li>
+                                <li><span className="ability">+Lava phoenix meteors can pop all Bloon types</span></li>
+                                <li><span className="ability">Ability cooldown: 45s</span></li>
+                                <li>Fireball explosion damage 3 -&gt; 9</li>
+                                <li>Wall of fire damage 1 -&gt; 5</li>
+                                <li>Now fires 3 Dragon's Breath flame projectiles/attack</li>
+                                <li>Dragon's Breath attack pierce 3 -&gt; 15</li>
+                                <li>Dragon's Breath damage over time increased to 20 damage/s</li>
+                                <li>Dragon's Breath damage over time duration 3s -&gt; 15s</li>
+                            </ul>
+                        </td>
                     </tr>
                 </tbody>
             </table>
@@ -126,12 +210,12 @@ export function WizardMain() {
             <table className="upgrade_table">
                 <thead>
                     <tr>
-                        <th className="top_row">Bottom Upgrade Path</th>
-                        <th className="top_row">Intense Magic</th>
-                        <th className="top_row">Monkey Sense</th>
-                        <th className="top_row">Shimmer</th>
-                        <th className="top_row">Necromancer: Unpopped Army</th>
-                        <th className="top_row">Prince of Darkness</th>
+                        <th className="top_row path_label">Bottom Upgrade Path</th>
+                        <th className="top_row upgrade_button">Intense Magic</th>
+                        <th className="top_row upgrade_button">Monkey Sense</th>
+                        <th className="top_row upgrade_button">Shimmer</th>
+                        <th className="top_row upgrade_button">Necromancer: Unpopped Army</th>
+                        <th className="top_row upgrade_button last_upgrade_button">Prince of Darkness</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -145,11 +229,56 @@ export function WizardMain() {
                     </tr>
                     <tr>
                         <th>Changes</th>
-                        <td>To be added</td>
-                        <td>To be added</td>
-                        <td>To be added</td>
-                        <td>To be added</td>
-                        <td>To be added</td>
+                        <td className="change_cell">
+                            <ul className="change_list">
+                                <li>Pierce 3 -&gt; 8</li>
+                                <li>Doubled projectile speed</li>
+                            </ul>
+                        </td>
+                        <td className="change_cell">
+                            <ul className="change_list">
+                                <li>+10 range</li>
+                                <li>+Camo detection</li>
+                            </ul>
+                        </td>
+                        <td className="change_cell">
+                            <ul className="change_list">
+                                <li><i>+Shimmer attack: removes Camo from up to 500 Bloons in 75-unit radius every 2s</i></li>
+                                <li>+10 range</li>
+                            </ul>
+                        </td>
+                        <td className="change_cell">
+                            <ul className="change_list">
+                                <li><i>+Graveyard mechanic: for every 1 Bloon layer popped within 70 units of Wizard (a little farther
+                                     than its display range), add 1 point to the Graveyard counter (7-10 points/layer after Round 80).
+                                      Graveyard maxed at 500 points</i></li>
+                                <li>All Wizard monkey attacks gain +1 damage for every 200 Graveyard points</li>
+                                <li><i>+Undead Bloons attack: summon group of 1-4 undead Bloons every 1.5s (at cost of 1-10 Graveyard
+                                     points)—undead Bloons travel backwards on track at speed of Red Bloons</i></li>
+                                <li><i>Undead Bloons summon +10% faster for every 100 Graveyard points in store</i></li>
+                                <li><i>Undead Bloons automatically spawn at locations closest to exit, though player can choose a
+                                     different position on targeting menu</i></li>
+                                <li><i>Undead Bloon stats: 2 damage, 2-11 pierce (depending on Graveyard points spent to summon this
+                                     Undead Bloon), 7s lifespan</i></li>
+                                <li>+Undead Bloons can pop ALL Bloon types</li>
+                            </ul>
+                        </td>
+                        <td className="change_cell">
+                            <ul className="change_list">
+                                <li><i>Graveyard capacity 500 points -&gt; 3,000 points</i></li>
+                                <li>Wizard monkey attacks instead gain +1 damage for every 300 stored Graveyard points instead of 200</li>
+                                <li><i>+Can now summon undead MOABs and BFBs: can summon MOABs when Graveyard point counter is between
+                                     20-2099 and BFBs when Graveyard point counter is 2100 or higher</i></li>
+                                <li><i>Undead MOAB stats: 43 damage, 20 pierce, 3s summon cooldown, 20 Graveyard point cost,
+                                     15s lifespan</i></li>
+                                <li><i>Undead BFB stats: 103 damage, 50 pierce, 3s summon cooldown, 50 Graveyard point cost,
+                                     10s lifespan</i></li>
+                                <li><i>+Necromancy buff: Prince of Darkness and all Necromancers deal +3 extra damage from
+                                     Undead Bloons</i></li>
+                                <li>Magic bolt (base attack) cooldown 1.1s -&gt; 0.275s</li>
+                                <li><i>Shimmer attack cooldown 2s -&gt; 1s</i></li>
+                            </ul>
+                        </td>
                     </tr>
                 </tbody>
             </table>
