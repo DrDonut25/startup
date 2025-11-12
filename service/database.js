74e55b5ec0c122711bb3aadc281db8139d068ca2
DrDonut25 <remundparker@gmail.com>
2025-11-11 22:03:02 -0700

    Fully implemented database.js (I think) with connection test function, db access methods, and db update methods

--- service/database.js
+++ service/database.js
@@ -4,4 +4,32 @@ const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostna
 const client = new MongoClient(url);
 
 const db = client.db('startup');
-const userCollection = db.collection('users');
+const userCollection = db.collection('startup_user');
+
+//db connection test
+(async function testConnection() {
+  try {
+    await db.command({ ping: 1 });
+  } catch (ex) {
+    console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+    process.exit(1);
+  }
+})();
+
+//db access methods
+function getUserByEmail(email) {
+    return userCollection.findOne({ email: email });
+}
+
+function getUserByToken(token) {
+    return userCollection.findOne({ token: token });
+}
+
+//db update methods
+async function addUser(user) {
+    await userCollection.insertOne(user);
+}
+
+async function updateUser(user) {
+  await userCollection.updateOne({ email: user.email }, { $set: user });
+}
