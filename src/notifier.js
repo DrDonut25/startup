2e2213f87a92256a86ea9393160c92e0c61a49f6
DrDonut25 <remundparker@gmail.com>
2025-12-01 16:10:49 -0700

    Added websocket configuration to vite; also added initialization statement of websocket to notifier.js

---
+++ src/notifier.js
@@ -0,0 +1,10 @@
+class EventNotifier {
+    events = [];
+    handlers = [];
+
+    constructor() {
+        let port = window.location.port;
+        const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
+        this.socket = new WebSocket(`${protocol}://${window.location.hostname}:${port}/ws`);
+    }
+}
