43207719de02d3f72202e29c8369ef41870faff9
DrDonut25 <remundparker@gmail.com>
2025-12-03 11:47:20 -0700

    Added connection/message handler methods to server-side of websocket, ping-pong handling to be done next

--- service/peerProxy.js
+++ service/peerProxy.js
@@ -1,2 +1,29 @@
 const { WebSocketServer } = require('ws');
 
+function peerProxy(httpServer) {
+    const wss = new WebSocketServer({ server: httpServer });
+
+    wss.on('connection', (ws) => {
+        ws.isAlive = true;
+
+        //message handler -> send to other clients (but only ones that are logged in)
+        ws.on('message', (data) => {
+            wss.clients.forEach((client) => {
+                //check that client is either not current user and that client is currently logged in
+                if (client !== ws && client.readyState === WebSocket.OPEN) {
+                    ws.send(data);
+                }
+            });
+        });
+
+        //ping handler
+        ws.on('pong', () => {
+            ws.isAlive = true;
+        })
+
+        //ping logged-in clients to ensure they're still active
+        
+    });
+}
+
+module.exports = { peerProxy };
